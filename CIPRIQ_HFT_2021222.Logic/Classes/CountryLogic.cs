15c9961791a78411eb165b54b948fbf952a1ebef
Zsanett Hegyi <mixer000092@gmail.com>
2022-11-30 20:31:38 +0100

    CountryPhoneStats

--- CIPRIQ_HFT_2021222.Logic/Classes/CountryLogic.cs
+++ CIPRIQ_HFT_2021222.Logic/Classes/CountryLogic.cs
@@ -47,6 +47,41 @@ namespace CIPRIQ_HFT_2022231.Logic.Classes
                           select C;
             return Country.First();
         }
+        public IQueryable<CountryStat> CountryPhoneStats()
+        {
+            var Country = from C in repo.ReadAll()
+                          from B in C.Brands
+                          from P in B.Phones
+                          group P by C.name into g
+                          select new CountryStat() { name = g.Key, PhoneNumber = g.Count(), MostExpensive = g.Max(x => x.Price) };
+            return Country;
+        }
+        public IQueryable CountriesPhoneRam(int ram)
+        {
+            var country = from C in repo.ReadAll()
+                          from B in C.Brands
+                          from P in B.Phones
+                          where P.RAM >= ram
+                          select C;
+            return country;
+        }
+        public double CountryPhonesAvgStorage(string name)
+        {
+            var country = from C in repo.ReadAll()
+                          where C.name == name
+                          from B in C.Brands
+                          select B.Phones.Average(x => x.Storage);
+            return country.First();
+        }
+        public IQueryable<Phone> PhonesInCountry(string input)
+        {
+            var phones = from C in repo.ReadAll()
+                         where C.name == input
+                         from B in C.Brands
+                         from P in B.Phones
+                         select P;
+            return phones;
+        }
     }
 }
 
